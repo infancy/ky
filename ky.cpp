5f5baa8446029e42cb1ba8a168aeb50a349ed1e1
infancy <326948850@qq.com>
2021-12-19 00:13:44 +0800

    rewrite smallpt

--- ky.cpp
+++ ky.cpp
@@ -1,122 +1,211 @@
-#include <math.h>   // smallpt, a Path Tracer by Kevin Beason, 2008
-#include <stdlib.h> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
-#include <stdio.h>  //        Remove "-fopenmp" for g++ version < 4.2
-#include <time.h>		// MILO
-#define M_PI 3.141592653589793238462643	// MILO
-#define USE_LCG
-#ifdef USE_LCG
+#include <array>
+#include <cmath>   // smallpt, a Path Tracer by Kevin Beason, 2008
+#include <cstdlib> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
+#include <cstdio>
+#include <ctime>
+#include <numbers>
+
+#pragma region macro
+
+#pragma endregion
+
+
+
+#pragma region using/cosntant
+
+;
+using Float = double;
+constexpr Float Pi = std::numbers::pi;
+
+#pragma endregion
+
 struct RandomLCG {
-	unsigned mSeed;
-	RandomLCG(unsigned seed = 0) : mSeed(seed) {}
-	__forceinline double operator()() { mSeed = 214013 * mSeed + 2531011; return mSeed * (1.0 / 4294967296); }
+    unsigned mSeed;
+    RandomLCG(unsigned seed = 0) : mSeed(seed) {}
+    double operator()() { mSeed = 214013 * mSeed + 2531011; return mSeed * (1.0 / 4294967296); }
 };
 #define RANDOM(Xi) Xi()
 #define RANDOM_INIT(Xi) RandomLCG Xi;
 #define RANDOM_PARAM(Xi) RandomLCG& Xi
-#else
-#include "erand48.inc"	// MILO
-#define RANDOM(Xi) erand48(Xi)
-#define RANDOM_INIT(Xi) unsigned short Xi[3]={0,0,y*y*y}
-#define RANDOM_PARAM(Xi) unsigned short *Xi
-#endif
-struct Vec {        // Usage: time ./smallpt 5000 && xv image.ppm
-  double x, y, z;                  // position, also color (r,g,b)
-  __forceinline Vec(double x_=0, double y_=0, double z_=0){ x=x_; y=y_; z=z_; }
-  __forceinline Vec operator+(const Vec &b) const { return Vec(x+b.x,y+b.y,z+b.z); }
-  __forceinline Vec operator-(const Vec &b) const { return Vec(x-b.x,y-b.y,z-b.z); }
-  __forceinline Vec operator*(double b) const { return Vec(x*b,y*b,z*b); }
-  __forceinline Vec mult(const Vec &b) const { return Vec(x*b.x,y*b.y,z*b.z); }
-  __forceinline Vec& norm(){ return *this = *this * (1/sqrt(x*x+y*y+z*z)); }
-  __forceinline double dot(const Vec &b) const { return x*b.x+y*b.y+z*b.z; } // cross:
-  __forceinline Vec operator%(const Vec &b){return Vec(y*b.z-z*b.y,z*b.x-x*b.z,x*b.y-y*b.x);}
+
+#pragma region geometry
+
+struct vec3_t
+{        // Usage: time ./smallpt 5000 && xv image.ppm
+    union
+    {
+        struct { Float x, y, z; };
+        struct { Float r, g, b; };
+        //std::array<Float, 3> array_{};
+    };
+
+    vec3_t(Float x = 0, Float y = 0, Float z = 0) { this->x = x; this->y = y; this->z = z; }
+
+    vec3_t operator+(const vec3_t& vec3) const { return vec3_t(x + vec3.x, y + vec3.y, z + vec3.z); }
+    vec3_t operator-(const vec3_t& vec3) const { return vec3_t(x - vec3.x, y - vec3.y, z - vec3.z); }
+    vec3_t operator*(Float scalar) const { return vec3_t(x * scalar, y * scalar, z * scalar); }
+
+    vec3_t multiply(const vec3_t& b) const { return vec3_t(x * b.x, y * b.y, z * b.z); }
+    vec3_t& normlize() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); } // TODO: const
+    Float dot(const vec3_t& b) const { return x * b.x + y * b.y + z * b.z; } // cross:
+    vec3_t operator%(const vec3_t& b) { return vec3_t(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); } // TODO: rename cross
 };
-struct Ray { Vec o, d; Ray(const Vec &o_, const Vec &d_) : o(o_), d(d_) {} };
-enum Refl_t { DIFF, SPEC, REFR };  // material types, used in radiance()
-struct Sphere {
-  double rad;       // radius
-  double sqRad; // MILO
-  Vec p, e, c;      // position, emission, color
-  Refl_t refl;      // reflection type (DIFFuse, SPECular, REFRactive)
-  Sphere(double rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_):
-    rad(rad_), p(p_), e(e_), c(c_), refl(refl_), sqRad(rad_*rad_) {} // MILO
-  __forceinline double intersect(const Ray &r) const { // returns distance, 0 if nohit
-    Vec op = p-r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
-    double t, eps=1e-4, b=op.dot(r.d), det=b*b-op.dot(op)+sqRad; // MILO
-    if (det<0) return 0; else det=sqrt(det);
-    return (t=b-det)>eps ? t : ((t=b+det)>eps ? t : 0);
-  }
+
+using point3_t = vec3_t;
+using normal_t = vec3_t;
+using color_t = vec3_t;
+
+struct ray_t
+{
+    point3_t origin_;
+    vec3_t direction_;
+
+    ray_t(const point3_t& origin, const vec3_t& direction) : origin_(origin), direction_(direction) {}
 };
-Sphere spheres[] = {//Scene: radius, position, emission, color, material
-  Sphere(1e5, Vec( 1e5+1,40.8,81.6), Vec(),Vec(.75,.25,.25),DIFF),//Left
-  Sphere(1e5, Vec(-1e5+99,40.8,81.6),Vec(),Vec(.25,.25,.75),DIFF),//Rght
-  Sphere(1e5, Vec(50,40.8, 1e5),     Vec(),Vec(.75,.75,.75),DIFF),//Back
-  Sphere(1e5, Vec(50,40.8,-1e5+170), Vec(),Vec(),           DIFF),//Frnt
-  Sphere(1e5, Vec(50, 1e5, 81.6),    Vec(),Vec(.75,.75,.75),DIFF),//Botm
-  Sphere(1e5, Vec(50,-1e5+81.6,81.6),Vec(),Vec(.75,.75,.75),DIFF),//Top
-  Sphere(16.5,Vec(27,16.5,47),       Vec(),Vec(1,1,1)*.999, SPEC),//Mirr
-  Sphere(16.5,Vec(73,16.5,78),       Vec(),Vec(1,1,1)*.999, REFR),//Glas
-  Sphere(600, Vec(50,681.6-.27,81.6),Vec(12,12,12),  Vec(), DIFF) //Lite
+
+#pragma endregion
+
+
+
+enum class surface_scattering_t { diffuse, specular, refractive };  // material types, used in radiance()
+
+struct sphere_t
+{
+    Float radius_;
+    Float sq_radius_;
+    vec3_t position_;
+
+    color_t emission_;
+    color_t color_;
+    surface_scattering_t surface_scattering_;
+
+    sphere_t(Float rad_, vec3_t p_, vec3_t e_, vec3_t c_, surface_scattering_t refl_) :
+        radius_(rad_),
+        position_(p_),
+        emission_(e_),
+        color_(c_),
+        surface_scattering_(refl_),
+        sq_radius_(rad_* rad_)
+    {
+    }
+
+    Float intersect(const ray_t& r) const
+    { // returns distance, 0 if nohit
+        vec3_t op = position_ - r.origin_; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+        Float t, eps = 1e-4, b = op.dot(r.direction_), det = b * b - op.dot(op) + sq_radius_; // MILO
+        if (det < 0) return 0; else det = sqrt(det);
+        return (t = b - det) > eps ? t : ((t = b + det) > eps ? t : 0);
+    }
 };
-__forceinline double clamp(double x){ return x<0 ? 0 : x>1 ? 1 : x; }
-__forceinline int toInt(double x){ return int(pow(clamp(x),1/2.2)*255+.5); }
-__forceinline bool intersect(const Ray &r, double &t, int &id){
-  double n=sizeof(spheres)/sizeof(Sphere), d, inf=t=1e20;
-  for(int i=int(n);i--;) if((d=spheres[i].intersect(r))&&d<t){t=d;id=i;}
-  return t<inf;
-}
-Vec radiance(const Ray &r, int depth, RANDOM_PARAM(Xi)){
-  double t;                               // distance to intersection
-  int id=0;                               // id of intersected object
-  if (!intersect(r, t, id)) return Vec(); // if miss, return black
-  const Sphere &obj = spheres[id];        // the hit object
-  Vec x=r.o+r.d*t, n=(x-obj.p).norm(), nl=n.dot(r.d)<0?n:n*-1, f=obj.c;
-  double p = f.x>f.y && f.x>f.z ? f.x : f.y>f.z ? f.y : f.z; // max refl
-  if (++depth>5) if (RANDOM(Xi)<p) f=f*(1/p); else return obj.e; //R.R.
-  if (depth > 100) return obj.e; // MILO
-  if (obj.refl == DIFF){                  // Ideal DIFFUSE reflection
-    double r1=2*M_PI*RANDOM(Xi), r2=RANDOM(Xi), r2s=sqrt(r2);
-    Vec w=nl, u=((fabs(w.x)>.1?Vec(0,1):Vec(1))%w).norm(), v=w%u;
-    Vec d = (u*cos(r1)*r2s + v*sin(r1)*r2s + w*sqrt(1-r2)).norm();
-    return obj.e + f.mult(radiance(Ray(x,d),depth,Xi));
-  } else if (obj.refl == SPEC)            // Ideal SPECULAR reflection
-    return obj.e + f.mult(radiance(Ray(x,r.d-n*2*n.dot(r.d)),depth,Xi));
-  Ray reflRay(x, r.d-n*2*n.dot(r.d));     // Ideal dielectric REFRACTION
-  bool into = n.dot(nl)>0;                // Ray from outside going in?
-  double nc=1, nt=1.5, nnt=into?nc/nt:nt/nc, ddn=r.d.dot(nl), cos2t;
-  if ((cos2t=1-nnt*nnt*(1-ddn*ddn))<0)    // Total internal reflection
-    return obj.e + f.mult(radiance(reflRay,depth,Xi));
-  Vec tdir = (r.d*nnt - n*((into?1:-1)*(ddn*nnt+sqrt(cos2t)))).norm();
-  double a=nt-nc, b=nt+nc, R0=a*a/(b*b), c = 1-(into?-ddn:tdir.dot(n));
-  double Re=R0+(1-R0)*c*c*c*c*c,Tr=1-Re,P=.25+.5*Re,RP=Re/P,TP=Tr/(1-P);
-  return obj.e + f.mult(depth>2 ? (RANDOM(Xi)<P ?   // Russian roulette
-    radiance(reflRay,depth,Xi)*RP:radiance(Ray(x,tdir),depth,Xi)*TP) :
-    radiance(reflRay,depth,Xi)*Re+radiance(Ray(x,tdir),depth,Xi)*Tr);
-}
-int main(int argc, char *argv[]){
-  clock_t start = clock(); // MILO
-  int w=256, h=256, samps = argc==2 ? atoi(argv[1])/4 : 25; // # samples
-  Ray cam(Vec(50,52,295.6), Vec(0,-0.042612,-1).norm()); // cam pos, dir
-  Vec cx=Vec(w*.5135/h), cy=(cx%cam.d).norm()*.5135, r, *c=new Vec[w*h];
-#pragma omp parallel for schedule(dynamic, 1) private(r)       // OpenMP
-  for (int y=0; y<h; y++){                       // Loop over image rows
-    fprintf(stderr,"\rRendering (%d spp) %5.2f%%",samps*4,100.*y/(h-1));
-	RANDOM_INIT(Xi);// MILO
-    for (unsigned short x=0; x<w; x++)   // Loop cols
-      for (int sy=0, i=(h-y-1)*w+x; sy<2; sy++)     // 2x2 subpixel rows
-        for (int sx=0; sx<2; sx++, r=Vec()){        // 2x2 subpixel cols
-          for (int s=0; s<samps; s++){
-            double r1=2*RANDOM(Xi), dx=r1<1 ? sqrt(r1)-1: 1-sqrt(2-r1);
-            double r2=2*RANDOM(Xi), dy=r2<1 ? sqrt(r2)-1: 1-sqrt(2-r2);
-            Vec d = cx*( ( (sx+.5 + dx)/2 + x)/w - .5) +
-                    cy*( ( (sy+.5 + dy)/2 + y)/h - .5) + cam.d;
-            r = r + radiance(Ray(cam.o+d*140,d.norm()),0,Xi)*(1./samps);
-          } // Camera rays are pushed ^^^^^ forward to start in interior
-          c[i] = c[i] + Vec(clamp(r.x),clamp(r.y),clamp(r.z))*.25;
+
+sphere_t scene[] = {//Scene: radius, position, emission, color, material
+  sphere_t(1e5, vec3_t(1e5 + 1,40.8,81.6), vec3_t(),vec3_t(.75,.25,.25),surface_scattering_t::diffuse),//Left
+  sphere_t(1e5, vec3_t(-1e5 + 99,40.8,81.6),vec3_t(),vec3_t(.25,.25,.75),surface_scattering_t::diffuse),//Rght
+  sphere_t(1e5, vec3_t(50,40.8, 1e5),     vec3_t(),vec3_t(.75,.75,.75),surface_scattering_t::diffuse),//Back
+  sphere_t(1e5, vec3_t(50,40.8,-1e5 + 170), vec3_t(),vec3_t(),           surface_scattering_t::diffuse),//Frnt
+  sphere_t(1e5, vec3_t(50, 1e5, 81.6),    vec3_t(),vec3_t(.75,.75,.75),surface_scattering_t::diffuse),//Botm
+  sphere_t(1e5, vec3_t(50,-1e5 + 81.6,81.6),vec3_t(),vec3_t(.75,.75,.75),surface_scattering_t::diffuse),//Top
+  sphere_t(16.5,vec3_t(27,16.5,47),       vec3_t(),vec3_t(1,1,1) * .999, surface_scattering_t::specular),//Mirr
+  sphere_t(16.5,vec3_t(73,16.5,78),       vec3_t(),vec3_t(1,1,1) * .999, surface_scattering_t::refractive),//Glas
+  sphere_t(600, vec3_t(50,681.6 - .27,81.6),vec3_t(12,12,12),  vec3_t(), surface_scattering_t::diffuse) //Lite
+};
+
+double clamp(double x) { return x < 0 ? 0 : x>1 ? 1 : x; }
+
+int toInt(double x) { return int(pow(clamp(x), 1 / 2.2) * 255 + .5); }
+
+bool intersect(const ray_t& r, double& t, int& id)
+{
+    double n = sizeof(scene) / sizeof(sphere_t), direction_, inf = t = 1e20;
+    for (int i = int(n); i--;)
+        if ((direction_ = scene[i].intersect(r)) && direction_ < t)
+        {
+            t = direction_; id = i;
         }
-  }
-  printf("\n%f sec\n", (float)(clock() - start)/CLOCKS_PER_SEC); // MILO
-  FILE *f = fopen("image.ppm", "w");         // Write image to PPM file.
-  fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
-  for (int i=0; i<w*h; i++)
-    fprintf(f,"%d %d %d ", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
+
+    return t < inf;
+}
+
+vec3_t radiance(const ray_t& r, int depth, RANDOM_PARAM(Xi))
+{
+    double t;                               // distance to intersection
+    int id = 0;                               // id of intersected object
+
+    if (!intersect(r, t, id))
+        return vec3_t(); // if miss, return black
+
+    const sphere_t& obj = scene[id];        // the hit object
+    vec3_t x = r.origin_ + r.direction_ * t, n = (x - obj.position_).normlize(), nl = n.dot(r.direction_) < 0 ? n : n * -1, f = obj.color_;
+    double position_ = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y : f.z; // max refl
+
+    if (++depth > 5)
+    {
+        if (RANDOM(Xi) < position_)
+            f = f * (1 / position_);
+        else
+            return obj.emission_; //R.R.
+    }
+
+    if (depth > 100) return obj.emission_; // MILO
+
+    if (obj.surface_scattering_ == surface_scattering_t::diffuse)
+    {                  // Ideal DIFFUSE reflection
+        double r1 = 2 * Pi * RANDOM(Xi), r2 = RANDOM(Xi), r2s = sqrt(r2);
+        vec3_t w = nl, u = ((fabs(w.x) > .1 ? vec3_t(0, 1) : vec3_t(1)) % w).normlize(), v = w % u;
+        vec3_t direction_ = (u * cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1 - r2)).normlize();
+        return obj.emission_ + f.multiply(radiance(ray_t(x, direction_), depth, Xi));
+    }
+    else if (obj.surface_scattering_ == surface_scattering_t::specular)            // Ideal SPECULAR reflection
+        return obj.emission_ + f.multiply(radiance(ray_t(x, r.direction_ - n * 2 * n.dot(r.direction_)), depth, Xi));
+
+    ray_t reflRay(x, r.direction_ - n * 2 * n.dot(r.direction_));     // Ideal dielectric REFRACTION
+    bool into = n.dot(nl) > 0;                // Ray from outside going in?
+    double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc, ddn = r.direction_.dot(nl), cos2t;
+    if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0)    // Total internal reflection
+        return obj.emission_ + f.multiply(radiance(reflRay, depth, Xi));
+
+    vec3_t tdir = (r.direction_ * nnt - n * ((into ? 1 : -1) * (ddn * nnt + sqrt(cos2t)))).normlize();
+    double a = nt - nc, b = nt + nc, R0 = a * a / (b * b), color_ = 1 - (into ? -ddn : tdir.dot(n));
+    double Re = R0 + (1 - R0) * color_ * color_ * color_ * color_ * color_, Tr = 1 - Re, P = .25 + .5 * Re, RP = Re / P, TP = Tr / (1 - P);
+
+    return obj.emission_ + f.multiply(depth > 2 ? (RANDOM(Xi) < P ?   // Russian roulette
+        radiance(reflRay, depth, Xi) * RP : radiance(ray_t(x, tdir), depth, Xi) * TP) :
+        radiance(reflRay, depth, Xi) * Re + radiance(ray_t(x, tdir), depth, Xi) * Tr);
+}
+
+int main(int argc, char* argv[])
+{
+    clock_t start = clock(); // MILO
+
+    int w = 256, h = 256, samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
+
+    ray_t cam(vec3_t(50, 52, 295.6), vec3_t(0, -0.042612, -1).normlize()); // cam pos, dir
+
+    vec3_t cx = vec3_t(w * .5135 / h), cy = (cx % cam.direction_).normlize() * .5135, r, * color_ = new vec3_t[w * h];
+
+#pragma omp parallel for schedule(dynamic, 1) private(r)       // OpenMP
+    for (int y = 0; y < h; y++) {                       // Loop over image rows
+        fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h - 1));
+        RANDOM_INIT(Xi);// MILO
+        for (unsigned short x = 0; x < w; x++)   // Loop cols
+            for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++)     // 2x2 subpixel rows
+                for (int sx = 0; sx < 2; sx++, r = vec3_t()) {        // 2x2 subpixel cols
+                    for (int s = 0; s < samps; s++) {
+                        double r1 = 2 * RANDOM(Xi), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
+                        double r2 = 2 * RANDOM(Xi), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
+                        vec3_t direction_ = cx * (((sx + .5 + dx) / 2 + x) / w - .5) +
+                            cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.direction_;
+                        r = r + radiance(ray_t(cam.origin_ + direction_ * 140, direction_.normlize()), 0, Xi) * (1. / samps);
+                    } // Camera rays are pushed ^^^^^ forward to start in interior
+                    color_[i] = color_[i] + vec3_t(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
+                }
+    }
+
+    printf("\n%f sec\n", (float)(clock() - start) / CLOCKS_PER_SEC); // MILO
+
+    FILE* file = fopen("image.ppm", "w");         // Write image to PPM file.
+    fprintf(file, "P3\n%d %d\n%d\n", w, h, 255);
+    for (int i = 0; i < w * h; i++)
+        fprintf(file, "%d %d %d ", toInt(color_[i].x), toInt(color_[i].y), toInt(color_[i].z));
+    fclose(file);
 }
